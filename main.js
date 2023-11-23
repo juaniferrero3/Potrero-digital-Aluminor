fb1dce4a1f91b5aa6cd40f0a0b9f4f5953696b1f
Juan Ignacio Ferrero <juaniferrero3@gmail.com>
2023-11-23 09:47:50 -0300

    agregando js

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const jsConfetti = new JSConfetti()
+
+    jsConfetti.addConfetti({
+        emojis: ['🔧', '⚡️', '💥', '⚒️', '-🚪', '🪟'],
+    })
+})
